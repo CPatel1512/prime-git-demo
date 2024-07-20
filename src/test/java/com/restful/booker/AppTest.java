76429437f798e1b50d54dfa2131be39215ceda18
chetan151281 <chetanpatel151281@gmail.com>
2024-07-20 15:11:05 +0100

    prime-git-demo project

---
+++ src/test/java/com/restful/booker/AppTest.java
@@ -0,0 +1,14 @@
+package com.restful.booker;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+    }
+}
